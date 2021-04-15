ed62fe05b06918f245b9871de87103b6aa51f40c
128880949@qq.com <orochi101119>
2021-04-15 14:35:53 +0800

    备份

---
+++ .svn/pristine/b3/b3e7387ee7c4fe6e395ca01d83efe44c8c273862.svn-base
@@ -0,0 +1,5 @@
+
+import YHBG_1 from './data_yhbg_1.js' // 验货报告_1
+import YHBG_2 from './data_yhbg_2.js' // 验货报告_2
+
+export default Object.assign({}, YHBG_1, YHBG_2)
---
+++ src/localData/data_yhbg_2.js
@@ -0,0 +1,328 @@
+/**
+ * [验货报告2]
+ */
+
+const obj = {
+  'businessPostList': [{
+    'business_post_id': '2c9f10b66662c55f016662d7a9db0000',
+    'empList': [{
+      'business_post_id': '2c9f10b66662c55f016662d7a9db0000',
+      'employee_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+      'employeename': '架构师',
+      'item_id': '2c9f10b67751442e01775255912e01f0',
+      'post_name': '服装业务'
+    }],
+    'post_name': '服装业务'
+  }, {
+    'business_post_id': '2c9f10b66662c55f016662daed1c0001',
+    'empList': [{
+      'business_post_id': '2c9f10b66662c55f016662daed1c0001',
+      'employee_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+      'employeename': '架构师',
+      'item_id': '2c9f10b67751442e01775255912e01f0',
+      'post_name': '面料业务'
+    }],
+    'post_name': '面料业务'
+  }, {
+    'business_post_id': '2c9f10b66662c55f016662db34410003',
+    'empList': [{
+      'business_post_id': '2c9f10b66662c55f016662db34410003',
+      'employee_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+      'employeename': '架构师',
+      'item_id': '2c9f10b67751442e01775255912e01f0',
+      'post_name': 'QC跟单'
+    }],
+    'post_name': 'QC跟单'
+  }, {
+    'business_post_id': '2c9f10b6673179830167358308560874',
+    'empList': [{
+      'business_post_id': '2c9f10b6673179830167358308560874',
+      'employee_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+      'employeename': '架构师',
+      'item_id': '2c9f10b67751442e01775255912e01f0',
+      'post_name': '开发业务'
+    }],
+    'post_name': '开发业务'
+  }, {
+    'business_post_id': '2c9f10b669c757740169dbccf56576a2',
+    'empList': [{
+      'business_post_id': '2c9f10b669c757740169dbccf56576a2',
+      'employee_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+      'employeename': '架构师',
+      'item_id': '2c9f10b67751442e01775255912e01f0',
+      'post_name': '技术版型'
+    }],
+    'post_name': '技术版型'
+  }, {
+    'business_post_id': '2c9f10b66b9ea321016bab174d0b2c14',
+    'empList': [{
+      'business_post_id': '2c9f10b66b9ea321016bab174d0b2c14',
+      'employee_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+      'employeename': '架构师',
+      'item_id': '2c9f10b67751442e01775255912e01f0',
+      'post_name': '技术单耗'
+    }],
+    'post_name': '技术单耗'
+  }, {
+    'business_post_id': '2c9f10b6739070c90173fa99738369dc',
+    'empList': [{
+      'business_post_id': '2c9f10b6739070c90173fa99738369dc',
+      'employee_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+      'employeename': '架构师',
+      'item_id': '2c9f10b67751442e01775255912e01f0',
+      'post_name': '录单人'
+    }],
+    'post_name': '录单人'
+  }],
+  'completion_method': '1',
+  'customMap': {
+    'custom_id': '9e773b0660444d1b8b672019d93b2079',
+    'custom_name': 'SLT-BS',
+    'delivery_type': '3'
+  },
+  'examineBusinessPostList': [{
+    'business_post_id': '2c9f10b669c757740169dbccf56576a2',
+    'examineBusinessEmpList': [{
+      'business_post_id': '2c9f10b669c757740169dbccf56576a2',
+      'emp_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+      'emp_name': '架构师',
+      'examine_emp_id': '2c9f10b677525c0601775699a36c036c',
+      'examine_goods_id': 'bc90344635b542aab3d874363973ee5b',
+      'examine_post_id': '4d254b4441cf4f869719248e06ee6fc3',
+      'ratio': 100
+    }],
+    'examine_goods_id': 'bc90344635b542aab3d874363973ee5b',
+    'examine_post_id': '4d254b4441cf4f869719248e06ee6fc3',
+    'is_delete': '1',
+    'out_in': '1',
+    'post_name': '技术版型',
+    'ratio': 20
+  }, {
+    'business_post_id': '2c9f10b66662c55f016662d7a9db0000',
+    'examineBusinessEmpList': [{
+      'business_post_id': '2c9f10b66662c55f016662d7a9db0000',
+      'emp_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+      'emp_name': '架构师',
+      'examine_emp_id': '2c9f10b677525c0601775699a36c036b',
+      'examine_goods_id': 'bc90344635b542aab3d874363973ee5b',
+      'examine_post_id': 'a5c85f11bdb64fabb2af1798c46dcd68',
+      'ratio': 100
+    }],
+    'examine_goods_id': 'bc90344635b542aab3d874363973ee5b',
+    'examine_post_id': 'a5c85f11bdb64fabb2af1798c46dcd68',
+    'is_delete': '1',
+    'out_in': '1',
+    'post_name': '服装业务',
+    'ratio': 80
+  }, {
+    'business_post_id': '',
+    'examineBusinessEmpList': [],
+    'examine_goods_id': 'bc90344635b542aab3d874363973ee5b',
+    'examine_post_id': 'ef4c3f79c0654355844f1a82f4cca752',
+    'is_delete': '1',
+    'out_in': '2',
+    'post_name': 'ETC',
+    'ratio': 100
+  }],
+  'examineGoods': {
+    'create_time': '2021-01-31 12:00:44',
+    'creator': '架构师',
+    'creator_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+    'custom_id': '9e773b0660444d1b8b672019d93b2079',
+    'deliver_type': '3',
+    'delivery_type': '3',
+    'examine_goods_id': 'bc90344635b542aab3d874363973ee5b',
+    'examine_time': '2022-01-03',
+    'fileList': [],
+    'final_examine_result': '1',
+    'final_explain': '总体合格',
+    'inside_ratio': 90,
+    'is_delete': '1',
+    'is_finally': '1',
+    'item_id': '2c9f10b67751442e01775255912e01f0',
+    'item_information': '业务拓展B组 > testdh01 > 21-春 > INDIGO休闲系 > 夹克 > SLIM > testdh01',
+    'outside_ratio': 10,
+    'remark': null,
+    'state': '1',
+    'update_time': '2021-01-31 12:00:44',
+    'updator': '架构师',
+    'updator_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+    'version_no': 1
+  },
+  'examineGoodsDetailList': [{
+    'create_time': '2021-01-31 12:00:44',
+    'creator': '架构师',
+    'creator_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+    'custom_store_id': '2c9f10b67499125701749a061ecf0135',
+    'dispatch_order_id': '846ab15af827478aa66ad9fa7db3786d',
+    'examineGoodsDetailDutyList': [],
+    'examine_detail_id': '57f016adf16c43efb627ee6895f9da62',
+    'examine_name': '00',
+    'examine_result': '1',
+    'examine_time': '2022-01-04',
+    'fileList': [{
+      'acce_id': '6699a70d6f4645c58bec33d0dcc93421',
+      'file_name': 'DSC_0004.JPG',
+      'file_path': '/upload/examineGoods/fc6ea48cbab24c67ad8b1a3f9f4ff978.JPG',
+      'is_pic': 1,
+      'thumb_path': '/upload/examineGoods/fc6ea48cbab24c67ad8b1a3f9f4ff978_thumb.JPG'
+    }],
+    'handle_mode': '',
+    'is_delete': '1',
+    'remark': null,
+    'store_name': '客户指定位置',
+    'update_time': '2021-01-31 12:00:44',
+    'updator': '架构师',
+    'updator_id': '965BAD8F4EF5C14CE4F607E77D30B9B5'
+  }, {
+    'create_time': '2021-01-31 12:00:45',
+    'creator': '架构师',
+    'creator_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+    'custom_store_id': '2c9f10b67499125701749a061ecf0135',
+    'dispatch_order_id': '',
+    'examineGoodsDetailDutyList': [{
+      'detail_explain': '',
+      'examine_detail_duty_id': '2c9f10b677525c0601775699a35e036a',
+      'examine_detail_id': 'BFDDAB571B98D6730159CC69F1439564',
+      'examine_goods_id': 'bc90344635b542aab3d874363973ee5b',
+      'in_post_id': '2c9f10b66662c55f016662d7a9db0000,2c9f10b669c757740169dbccf56576a2',
+      'out_code': 'ETC',
+      'ratio': 100,
+      'reason_desc': '8a8a80b6771d3bf301771de138c80012',
+      'reason_type': '8a8a80b6771d3bf301771de138c70011'
+    }],
+    'examine_detail_id': 'BFDDAB571B98D6730159CC69F1439564',
+    'examine_name': 'ee',
+    'examine_result': '0',
+    'examine_time': '2022-01-03',
+    'fileList': [{
+      'acce_id': '732c0268ba4e4f5aabd91f78c6cc2a84',
+      'file_name': 'DSC_0004.JPG',
+      'file_path': '/upload/examineGoods/55f48711c4b044139f9270a4bf693313.JPG',
+      'is_pic': 1,
+      'thumb_path': '/upload/examineGoods/55f48711c4b044139f9270a4bf693313_thumb.JPG'
+    }],
+    'handle_mode': '退步接受',
+    'is_delete': '1',
+    'remark': '',
+    'store_name': '客户指定位置',
+    'update_time': '2021-01-31 12:00:45',
+    'updator': '架构师',
+    'updator_id': '965BAD8F4EF5C14CE4F607E77D30B9B5'
+  }],
+  'examineNoQualifyList': [{
+    'create_time': '2021-01-20 11:40:35',
+    'creator': '架构师',
+    'creator_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+    'detailList': [{
+      'name': '描述1',
+      'qualify_detail_id': '8a8a80b6771d3bf301771de138c80012',
+      'qualify_id': '8a8a80b6771d3bf301771de138c70011',
+      'sort': 1
+    }, {
+      'name': '描述2',
+      'qualify_detail_id': '8a8a80b6771d3bf301771de138c90013',
+      'qualify_id': '8a8a80b6771d3bf301771de138c70011',
+      'sort': 2
+    }],
+    'is_delete': '1',
+    'name': '类型1',
+    'qualify_id': '8a8a80b6771d3bf301771de138c70011',
+    'remark': null,
+    'update_time': '2021-01-20 11:40:35',
+    'updator': '架构师',
+    'updator_id': '965BAD8F4EF5C14CE4F607E77D30B9B5'
+  }, {
+    'create_time': '2021-01-20 11:40:51',
+    'creator': '架构师',
+    'creator_id': '965BAD8F4EF5C14CE4F607E77D30B9B5',
+    'detailList': [{
+      'name': '描述3',
+      'qualify_detail_id': '8a8a80b6771d3bf301771de177370015',
+      'qualify_id': '8a8a80b6771d3bf301771de177360014',
+      'sort': 3
+    }, {
+      'name': '描述4',
+      'qualify_detail_id': '8a8a80b6771d3bf301771de177380016',
+      'qualify_id': '8a8a80b6771d3bf301771de177360014',
+      'sort': 4
+    }],
+    'is_delete': '1',
+    'name': '类型2',
+    'qualify_id': '8a8a80b6771d3bf301771de177360014',
+    'remark': null,
+    'update_time': '2021-01-20 11:40:51',
+    'updator': '架构师',
+    'updator_id': '965BAD8F4EF5C14CE4F607E77D30B9B5'
+  }],
+  'wbzrgs': [{
+    'dcid': '2c9f10b67689c0dd017689d259330040',
+    'dcode': '客户',
+    'dcvalue': '客户',
+    'dictionary': null,
+    'orderno': 1,
+    'remark': '',
+    'typecode': 'wbzrgs'
+  }, {
+    'dcid': '2c9f10b67689c0dd017689d259330041',
+    'dcode': 'ETC',
+    'dcvalue': 'ETC',
+    'dictionary': null,
+    'orderno': 2,
+    'remark': '',
+    'typecode': 'wbzrgs'
+  }, {
+    'dcid': '2c9f10b67689c0dd017689d259330042',
+    'dcode': '其他',
+    'dcvalue': '其他',
+    'dictionary': null,
+    'orderno': 3,
+    'remark': '',
+    'typecode': 'wbzrgs'
+  }],
+  'yhclfs': [{
+    'dcid': '2c9f10b67689c0dd017689d31efe0049',
+    'dcode': '退步接受',
+    'dcvalue': '退步接受',
+    'dictionary': null,
+    'orderno': 1,
+    'remark': '',
+    'typecode': 'yhclfs'
+  }, {
+    'dcid': '2c9f10b67689c0dd017689d31efe004a',
+    'dcode': '退厂返修',
+    'dcvalue': '退厂返修',
+    'dictionary': null,
+    'orderno': 2,
+    'remark': '',
+    'typecode': 'yhclfs'
+  }, {
+    'dcid': '2c9f10b67689c0dd017689d31efe004b',
+    'dcode': '试卖',
+    'dcvalue': '试卖',
+    'dictionary': null,
+    'orderno': 3,
+    'remark': '',
+    'typecode': 'yhclfs'
+  }, {
+    'dcid': '2c9f10b67689c0dd017689d31efe004c',
+    'dcode': '退货',
+    'dcvalue': '退货',
+    'dictionary': null,
+    'orderno': 4,
+    'remark': '',
+    'typecode': 'yhclfs'
+  }, {
+    'dcid': '2c9f10b67689c0dd017689d31efe004d',
+    'dcode': '折扣',
+    'dcvalue': '折扣',
+    'dictionary': null,
+    'orderno': 5,
+    'remark': '',
+    'typecode': 'yhclfs'
+  }]
+}
+
+export default {
+  '验货报告_2': obj
+}
